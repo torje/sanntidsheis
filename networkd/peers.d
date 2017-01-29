00fd68ad19eb46b347e467778ccbb4a47ad8a10d
Torje Hoås Digernes <torjehoa@pvv.org>
2017-01-29 12:49:33 +0100

    * intraprocess communication up, but not modulized * simulator communication works, tcp connections work. * udp not tested

---
+++ networkd/peers.d
@@ -0,0 +1,142 @@
+import  core.thread,
+        std.algorithm,
+        std.array,
+        std.concurrency,
+        std.conv,
+        std.datetime,
+        std.file,
+        std.getopt,
+        std.socket,
+        std.stdio;
+
+
+private __gshared ushort        port            = 16567;
+private __gshared int           timeout_ms      = 350;
+private __gshared Duration      timeout;        
+private __gshared int           interval_ms     = 100;
+private __gshared Duration      interval;       
+private __gshared string        id_str          = "default";
+private __gshared ubyte         _id;
+
+ubyte id(){
+    return _id;
+}
+
+
+struct PeerList {
+    immutable(ubyte)[] peers;
+    alias peers this;
+}
+
+struct TxEnable {
+    bool enable;
+    alias enable this;
+}
+
+Tid init(Tid receiver = thisTid){
+
+    string[] configContents;
+    try {
+        configContents = readText("net.con").split;
+        getopt( configContents,
+            std.getopt.config.passThrough,
+            "net_peer_port",        &port,
+            "net_peer_timeout",     &timeout_ms,
+            "net_peer_interval",    &interval_ms,
+            "net_peer_id",          &id_str
+        );
+        
+            
+        timeout = timeout_ms.msecs;
+        interval = interval_ms.msecs;
+        
+        if(id_str == "default"){ 
+            _id = new TcpSocket(new InternetAddress("google.com", 80))
+                .localAddress
+                .toAddrString
+                .splitter('.')
+                .array[$-1]
+                .to!ubyte;
+        } else {
+            _id = id_str.to!ubyte;
+        }
+        
+    } catch(Exception e){
+        writeln("Unable to load net_peer config:\n", e.msg);
+    }
+
+    
+    spawn(&rx, receiver);
+    return spawn(&tx);
+}
+
+
+
+private void tx(){
+    scope(exit) writeln(__FUNCTION__, " died");
+    try {
+
+    auto    addr                    = new InternetAddress("255.255.255.255", port);
+    auto    sock                    = new UdpSocket();
+    ubyte[1] buf                    = [id];
+
+    sock.setOption(SocketOptionLevel.SOCKET, SocketOption.BROADCAST, 1);
+    sock.setOption(SocketOptionLevel.SOCKET, SocketOption.REUSEADDR, 1);
+
+    bool txEnable = true;
+    while(true){
+        receiveTimeout(interval, 
+            (TxEnable t){
+                txEnable = t;
+            }
+        );
+        if(txEnable){
+            sock.sendTo(buf, addr);
+        }
+    }
+    } catch(Throwable t){ t.writeln; throw t; }
+}
+
+private void rx(Tid receiver){
+    scope(exit) writeln(__FUNCTION__, " died");
+    try {
+
+    auto    addr                    = new InternetAddress(port);
+    auto    sock                    = new UdpSocket();
+
+    ubyte[1]        buf;
+    SysTime[ubyte]  lastSeen;
+    bool            listHasChanges;
+
+
+    sock.setOption(SocketOptionLevel.SOCKET, SocketOption.BROADCAST, 1);
+    sock.setOption(SocketOptionLevel.SOCKET, SocketOption.REUSEADDR, 1);
+    sock.setOption(SocketOptionLevel.SOCKET, SocketOption.RCVTIMEO, timeout);
+    sock.bind(addr);
+
+    while(true){
+        listHasChanges  = false;
+        buf[]           = 0;
+
+        sock.receiveFrom(buf);
+
+        if(buf[0] != 0){
+            if(buf[0] !in lastSeen){
+                listHasChanges = true;
+            }
+            lastSeen[buf[0]] = Clock.currTime;
+        }
+
+        foreach(k, v; lastSeen){
+            if(Clock.currTime - v > timeout){
+                listHasChanges = true;
+                lastSeen.remove(k);
+            }
+        }
+
+        if(listHasChanges){
+            ownerTid.send(PeerList(lastSeen.keys.idup));
+        }
+    }
+    } catch(Throwable t){ t.writeln; throw t; }
+}
--- stage1.d
+++ stage1.d
@@ -1,3 +1,117 @@
-import std.stdio, std.concurrency;
+import std.stdio, std.concurrency, std.socket, std.conv, std.algorithm, std.typecons;
+import core.thread, core.time;
 
-void main()
+struct message{
+  bool ismsg=false;
+  ubyte[] msg;
+  bool opCast(T:bool)(){
+    return ismsg;
+  }
+  this( ubyte[] msg){
+    this.msg = msg.dup;
+    ismsg =true;
+  }
+  string toString(){
+    return std.conv.to!string(this.msg);
+  }
+  }/+
+  bool to(T:bool)(message msg){
+  return msg.ismsg;
+  }+/
+
+  void printElevatorStatus(TcpSocket tcp){
+    ubyte[] reads= [6:10];
+    ubyte[] floors = [0:4];
+    ubyte[] buttons = [0:3];
+    foreach( floor; floors){
+      foreach( button; buttons){
+        ubyte[] order= [reads[0], button,floor,0];
+        ubyte[] readback=[0,0,0,0];
+        write(order);
+        tcp.send(order);
+        tcp.receive(readback);
+        write(readback);
+      }
+    }
+
+  }
+  void communicator( Tid talkto){
+    auto address = getAddress("localhost", 15657);
+    writeln(address);
+    auto b = find!(a=>a.addressFamily()==AddressFamily.INET)( address);
+    writeln(b);
+    auto tcp = new TcpSocket(b[0]);
+    tcp.blocking= false;
+    while(true){
+      message toBeSent;
+      message* tbs=&toBeSent;
+      receiveTimeout(dur!"msecs"(0),
+      (ubyte a){
+        writeln("threadcom!: ",a);
+        },
+        ( Tuple!(ubyte,ubyte,ubyte,ubyte) data){
+          ubyte[] arr;
+          arr~=data[0];
+          arr~=data[1];
+          arr~=data[2];
+          arr~=data[3];
+
+          writeln("threadcom!: ",arr);
+          *tbs = message(arr);
+        }
+
+        );
+        if(toBeSent){
+          tcpSend(tcp,toBeSent.msg);
+      }
+      auto msg = readfrom(tcp);
+      if ( msg  ){
+        writeln("TCP received: ",msg);
+      }
+      Thread.sleep(dur!"msecs"(50) );
+    }
+    tcp.close();
+  }
+
+  void tcpSend(TcpSocket tcp, ubyte[] data){
+    //ubyte[] data = [6,0,0,0];
+    auto res = tcp.send(data);
+
+    writeln("sent data: ",data);
+  }
+
+  message readfrom(TcpSocket tcp){
+    ubyte[4] data;
+    auto res = tcp.receive(data);
+    if ( res == Socket.ERROR){
+      //writeln( "error on receive");
+      message msg;
+      return msg;
+    }else{
+      message msg = message(data);
+      //writeln("res: ",res);
+      //writeln("received data:" ,msg);
+      return msg;
+    }
+  }
+
+  void main(){
+    auto tid = spawn(&communicator,thisTid);
+    while (true)
+    {
+      //ubyte[] a=[4,4,4,5];
+      ubyte[] data = [6,0,0,0];
+
+      ubyte input = readf(" %s %s %s %s",&data[0],&data[1],&data[2],&data[3]).to!ubyte;
+
+      Tuple!(ubyte,ubyte,ubyte,ubyte) a=Tuple!(ubyte,ubyte,ubyte,ubyte)(6,0,0,0);
+      if ( data.length == 4){
+        a[0]=data[0];
+        a[1]=data[1];
+        a[2]=data[2];
+        a[3]=data[3];
+      }
+      send(tid,a);
+      Thread.sleep(dur!"msecs"(5000));
+    }
+  }
