053f7fb3e9cd2f482dfbb1002c1324155fba1e5c
Torje Hoås Digernes <torjehoa@pvv.org>
2017-03-04 12:50:48 +0100

    buttons

---
+++ input/button.d
@@ -0,0 +1,60 @@
+
+immutable int N_FLOORS = 4;
+
+// Number of buttons (and corresponding lamps) on a per-floor basis
+immutable N_BUTTONS =  3;
+enum elev_motor_direction_t:int{
+    DIRN_DOWN = -1,
+    DIRN_STOP = 0,
+    DIRN_UP = 1
+}
+enum elev_button_type_t:int{
+    BUTTON_CALL_UP = 0,
+    BUTTON_CALL_DOWN = 1,
+    BUTTON_COMMAND = 2
+}
+
+enum elev_type:int{
+    ET_Comedi,
+    ET_Simulation
+}
+
+extern(C){
+
+void elev_init(elev_type e);
+
+void elev_set_motor_direction(elev_motor_direction_t dirn);
+void elev_set_button_lamp(elev_button_type_t button, int floor, int value);
+void elev_set_floor_indicator(int floor);
+void elev_set_door_open_lamp(int value);
+void elev_set_stop_lamp(int value);
+
+int elev_get_button_signal(elev_button_type_t button, int floor);
+int elev_get_floor_sensor_signal();
+int elev_get_stop_signal();
+int elev_get_obstruction_signal();
+
+
+
+
+}
+import core.time, core.thread;
+import std.stdio;
+
+struct MultiState{
+    int function() foo;
+    this(  int function ()foo){
+        this.foo = foo;
+    }
+}
+
+void main(){
+    writeln("hello, World! ");
+    elev_init(elev_type.ET_Comedi);
+    while( true){
+        foreach(i; 0..N_FLOORS){
+            int atFloor = elev_get_floor_sensor_signal();
+            writeln("atFloor: ", atFloor);
+        }
+    }
+}
