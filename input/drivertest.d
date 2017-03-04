6b3c92332af8ebc8c04a0d4300185c74b86713c6
Torje Hoås Digernes <torjehoa@pvv.org>
2017-03-04 15:04:42 +0100

    automsg

--- input/drivertest.d
+++ input/drivertest.d
@@ -6,14 +6,14 @@ import std.concurrency;
 
 void main(){
 
-    elevator.init(elev_type.ET_Comedi);
+    /+elevator.init(elev_type.ET_Comedi);
     //MultiState floors = MultiState("floor: ");
     elevator.up();
     Thread.sleep(dur!"msecs"(250));
     elevator.stop();
     Thread.sleep(dur!"msecs"(250));
     elevator.down();
-    Thread.sleep(dur!"msecs"(250));
+    Thread.sleep(dur!"msecs"(250));+/
     elevator.stop();
     spawn(&elevator.spawn,elev_type.ET_Comedi);
     /+while( true){
