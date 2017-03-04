99587d2bbc116f9912b9242157d7b7f9194632e0
Torje Hoås Digernes <torjehoa@pvv.org>
2017-03-04 13:56:53 +0100

    foo

--- input/button.d
+++ input/button.d
@@ -43,18 +43,30 @@ import std.stdio;
 
 struct MultiState{
     int function() foo;
+    int state;
+    string msg;
+    //int (*foo)();
     this(  int function ()foo){
         this.foo = foo;
+    }
+    this (string msg){
+        this.msg = msg;
+    }
+    void update(int current){
+        if ( current != state){
+            writeln(msg,current);
+        }
+        state = current;
     }
 }
 
 void main(){
     writeln("hello, World! ");
     elev_init(elev_type.ET_Comedi);
+    MultiState floors = MultiState("floor: ");
     while( true){
         foreach(i; 0..N_FLOORS){
-            int atFloor = elev_get_floor_sensor_signal();
-            writeln("atFloor: ", atFloor);
+            floors.update( elev_get_floor_sensor_signal());
         }
     }
 }
